a39ade1b4ebbabbf734e68fd693b250e6824fcd0
refaat1297 <a.refaat1297@gmail.com>
2023-05-06 11:47:09 +0300

    add current repo

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+<!--    <NuxtWelcome />-->
+
+    <h2 class="bg-red-500">curret</h2>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.test {
+  //background-color: $mainColor;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  // https://bitbucket.org/${parsed.repo}/get/${parsed.ref}.tar.gz
+  // https://bitbucket.org/aklat/shared-repo/src/main/
+  // extends: "bitbucket:aklat/shared-repo/src/main/"
+  // extends: "bitbucket:refaat1297-aklat/shared-repo"
+  // extends: "bitbucket:aklat/shared-repo",
+  // extends: ["bitbucket:aklat/shared-repo"]
+  // modules: ['@nuxtjs/tailwindcss']
+  // extends: ['bitbucket:aklat/shared-repo']
+  // extends: ['github:refaat1297/arabia-meet']
+  // extends: ['github:LearnVueCo/nuxt-extends-demo']
+  // extends: ['github:refaat1297/arabia-meet']
+  extends: ['../../portfolio/arabia-meet']
+  // extends: '../shared-repo'
+})
